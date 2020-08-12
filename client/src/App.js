31b0de2d0c4f3e5cc45baf42e41e09884eb5d8ff
Twan Box <twanbox@gmail.com>
2020-08-12 12:14:53 +0200

    created react app & setup eslint

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          hello world!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
