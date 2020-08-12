5dce1ca83680e704c2a2db544033095866b3fe62
Twan Box <twanbox@gmail.com>
2020-08-12 15:21:37 +0200

    mock data served on api/news endpoint

--- server/news.js
+++ server/news.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const fs = require('fs');
-const router = express.Router();
-const news = process.env.NODE_ENV==='development' ? require('./db/development/news-dev.json') : require('./db/production/news-prod.json');
 
-router.get('/', (req, res) => res.send('Data'));
+const router = express.Router();
+const news = process.env.NODE_ENV === 'development'
+  ? './db/development/news-dev.json'
+  : './db/production/news-prod.json';
+
+router.get('/', (req, res) => {
+  fs.readFile(news, (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const news = require('./news.js')
+const news = require('./news.js');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(express.static());
-app.use('/api/news', news) 
+app.use('/api/news', news);
 
 app.listen(8080, () => console.log('Server started on http://localhost:8080'));
